29aa195a2d7f780a7af589a3719d5e0646e85df7
janarth10 <janarthp10@gmail.com>
2021-12-31 11:24:18 -0800

    first feature ready! save top10 songs weekly

--- app.py
+++ app.py
@@ -1,36 +1,45 @@
-import requests
 import datetime
+import json
+import requests
+
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
 
-user_id = "janarth.96"
-base_url = "https://api.spotify.com/v1/"
-# url = f"https://api.spotify.com/v1/users/me/playlists"
-access_token = "BQCr5tLF5qcST16T9zNxTfQ7kWNgZTh9QGMGt5rxa2y2cwS2UKAOwZ4ffJ6SAgW56NcvDWXXgg-YiJNmnhFaswVjadRKkGjPtqwcLK8rLuki1VxgyvJGsi3LzvIGftzHr1jEAIaQJhDpNzgPeqzT-0W63Sz25FoHguwa23_E1FR016ScQL_Iuco4dajjum6wNRiEFwgb8pBreQZz1w9un5Wl_LMQgRo"
+MY_USER_ID = "janarth.96"
+SPOTIFY_CREDENTIALS_PATH = "/Users/newdev/Hive/Development/personal_projects/spotify-liked-songs-manager/configs/spotify_credentials.json"
 
-def spotify_get_request(endpoint, params={}):
-	resp = requests.get(
-		base_url+endpoint,
-		headers={"Authorization": f"Bearer {access_token}" },
-		params=params
-	)
-	if not resp.ok:
-		raise(Exception(resp.text))
+SCOPES = [
+	# Listening History scopes
+	'user-top-read', # get TOP 50 tracks in last 4 months
 
-	return resp.json()
+	# Playlists scopes
+	'playlist-modify-private',
+	'playlist-read-private',
+	'playlist-modify-public',
+]
 
-def spotify_post_request(endpoint, request_json={}):
-	resp = requests.post(
-		base_url+endpoint,
-		headers={"Authorization": f"Bearer {access_token}" },
-		json=request_json
-	)
-	if not resp.ok:
-		raise(Exception(resp.text))
+def get_spotify_client():
+	# https://developer.spotify.com/documentation/general/guides/authorization/scopes/
+	web_api_creds = None
+	with open(SPOTIFY_CREDENTIALS_PATH) as f:
+		web_api_creds = json.load(f)['web']
 
-	return resp.json()
+	spotify_client = spotipy.Spotify(auth_manager=SpotifyOAuth(
+		scope=SCOPES,
+		client_id=web_api_creds['client_id'],
+		client_secret=web_api_creds['client_secret'],
+		redirect_uri='http://127.0.0.1:9090',
+		open_browser=True,
+	))
+	return spotify_client
 
 
-def save_top_10_weekly():
+def get_liked_songs():
+	# https://spotipy.readthedocs.io/en/2.19.0/?highlight=top#spotipy.client.Spotify.current_user_saved_tracks
+	pass
+
+def save_top10_weekly():
 	"""
 	Only able to fetch top X songs in last 4 weeks. solution
 
@@ -41,42 +50,52 @@ def save_top_10_weekly():
 		if any left over, save to new playlist
 	
 	Use cron to run this weekly
-
-	todos
-	- need to get access_token everytime this fn runs, with permissions to create, read, users_top
-	- add cron to correct folder on Macbook
-	- create first playlist manually
-	- run script to make sure 2nd week playlist doesn't have any from first
-
 	"""
 
 	PLAYLIST_NAME_BASE = 'TOP10_'
-	USERS_TOP_API = 'me/top/tracks'
 	# TOP10_Dec-26-2021
 	# TODO - this is wrong timezone
-	top_10_weekly_playlist_name = PLAYLIST_NAME_BASE + datetime.datetime.utcnow().astimezone().strftime('%b-%d-%Y')
+	top10_weekly_playlist_name = PLAYLIST_NAME_BASE + datetime.datetime.utcnow().astimezone().strftime('%b-%d-%Y')
 
-	# TODO - get new token
+	spotify_client = get_spotify_client()
+	top50_resp_json = spotify_client.current_user_top_tracks(limit=50, time_range='short_term')
+	current_top50_track_uris_set = {x['uri'] for x in top50_resp_json['items']}
+	print('\n\n')
+	print(f"found {len(current_top50_track_uris_set)} top50s")
 
-	resp_json = spotify_get_request(
-		endpoint=USERS_TOP_API,
-		params={'limit': 50, 'time_range': 'short_term'}
-	)
-	track_uris = [x['uri'] for x in resp_json['items']]
+	all_playlists_resp_json = spotify_client.current_user_playlists()
+	top10_playlist_ids = [
+		playlist['id'] for playlist in 
+		all_playlists_resp_json['items'] 
+		if playlist['name'][:len(PLAYLIST_NAME_BASE)] == PLAYLIST_NAME_BASE
+	]
 
-	print(track_uris)
-
-	# find previous top10s and filter out by URIs
-	
-	# resp_json = spotify_get_request(f"users/{user_id}/playlists")
-
-	# filter for playlist w names starting with 'TOP10_'
 	# only look at last 10 playlists if we have more than 10 TOP10_s
-	# playlist['tracks'] = {'href': 'https://api.spotify.com/v1/playlists/4a0Ev1qmduIWdcHmtKfPlX/tracks', 'total': 2}
-	# if total > 1 -> request url and get `old_track_uris`. Do a set difference
+	for playlist_id in top10_playlist_ids[:10]:
+		playlist_tracks_uri_set = {
+			track['track']['uri'] for track in 
+			spotify_client.playlist_items(
+				playlist_id=playlist_id, 
+				fields='items.track.uri'
+			)['items']
+		}
+		current_top50_track_uris_set = current_top50_track_uris_set - playlist_tracks_uri_set
 
-	# with leftover tracks in `track_uris`
-	# 1. create new playlist -> save ID
-	# 2. spotify_post_request(f"playlists/{new_playlist_id}/tracks", {'uris': track_uris})
+	# if we have any new tracks in the top50, create a new top10 playlist for them
+	if current_top50_track_uris_set:
+		new_top10_playlist = spotify_client.user_playlist_create(
+			user=MY_USER_ID,
+			name=top10_weekly_playlist_name,
+		)
+		print(f"creating new playlist {new_top10_playlist['name']}")
 
-save_top_10_weekly()
+		spotify_client.playlist_add_items(
+			playlist_id=new_top10_playlist['id'],
+			items=list(current_top50_track_uris_set)[:10] 
+		)
+		print(f"Spotify cron added {len(list(current_top50_track_uris_set)[:10])}")
+
+
+
+# ------------ Main Code - running weekly on a cron ----------------------------
+save_top10_weekly()
